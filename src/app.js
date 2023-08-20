d10371c726f8479758a03d0037fc5dc5e2b417f4
FerTorron <fertorron21@gmail.com>
2023-08-20 12:01:36 +0200

    Uploda project

---
+++ src/app.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import handlebars from 'express-handlebars'
+import __dirname from './utils.js'
+import viewsRouter from './routes/views.router.js'
+import { Server } from 'socket.io'
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/public'))
+
+
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars')
+
+app.use('/', viewsRouter)
+
+const server = app.listen(8080, () => {
+    console.log("Servidor ON")
+})
+
+const io = new Server(server)
+let messages = []
+
+io.on('connection', socket => {
+    console.log(`Nuevo cliente conectado ID: ${socket.id}`)
+
+    socket.on('message', data => {
+        messages.push(data)
+        io.emit('messageLogs', messages)
+    })
+
+    socket.on('authenticated', data => {
+        socket.broadcast.emit('newUserConnected', data)
+    })
+})
---
+++ src/public/js/index.js
@@ -0,0 +1,47 @@
+const socket = io()
+let user
+let chatBox = document.getElementById("chatBox")
+
+
+Swal.fire({
+    title: "¿Cómo te Llamas?",
+    input: "text",
+    text: "Ingresa tu Nombre de Usuario",
+    inputValidator: (value) => {
+        return !value && "Necesitas escribir un Nombre de Usuario"
+    },
+    allowOutsideClick: false
+}).then(result => {
+    user = result.value
+    socket.emit('authenticated', user)
+})
+
+chatBox.addEventListener('keyup', event => {
+    if (event.key === "Enter") {
+        if (chatBox.value.trim().length > 0) {
+            socket.emit("message", { user: user, message: chatBox.value })
+            chatBox.value = ""
+        }
+    }
+})
+
+socket.on('messageLogs', data => {
+    let log = document.getElementById("messageLogs")
+    let messages = ""
+    data.forEach(message => {
+        messages += `${message.user} dice: ${message.message} <br>`
+    });
+    log.innerHTML = messages
+})
+
+socket.on('newUserConnected', data => {
+    if (!user) return
+    Swal.fire({
+        toast: true,
+        position: "top-right",
+        title: `${data} se ha unido al Chat`,
+        timer: 2000,
+        showConfirmButton: false,
+        icon: "info"
+    })
+})
